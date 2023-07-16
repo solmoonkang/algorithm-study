c1102e08abdf9c802870cea555a6a7337f78170d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-07-16 18:22:02 +0900

    feat : 3주차

---
+++ src/codingTest/level0/day3/문자열섞기.java
@@ -0,0 +1,27 @@
+package codingTest.level0.day3;
+
+public class 문자열섞기 {
+
+    public String solution(String str1, String str2) {
+
+        String answer = "";
+
+        for (int i = 0; i < str1.length(); i++) {
+
+            answer += str1.substring(i, i + 1);
+            answer += str2.substring(i, i + 1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String str1 = "aaaaa";
+        String str2 = "bbbbb";
+
+        문자열섞기 mix = new 문자열섞기();
+
+        System.out.println("Result : " + mix.solution(str1, str2));
+    }
+}
