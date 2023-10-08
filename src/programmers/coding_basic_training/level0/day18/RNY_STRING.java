3514740da67c8130a4efc2c265e8be6304177cf5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-08 16:43:07 +0900

    feat : 코딩 기초 트레이닝 18주차

---
+++ src/programmers/coding_basic_training/level0/day18/RNY_STRING.java
@@ -0,0 +1,21 @@
+package programmers.coding_basic_training.level0.day18;
+
+public class RNY_STRING {
+
+    public String solution(String rny_string) {
+
+        // rny_string 의 모든 'm'을 'rn'으로 바꾼 문자열을 반환하라
+
+        return rny_string.replace("m", "rn");
+    }
+
+    public static void main(String[] args) {
+
+        RNY_STRING rnyString = new RNY_STRING();
+
+        System.out.println("Result : " + rnyString.solution("masterpiece"));
+        System.out.println("Result : " + rnyString.solution("programmers"));
+        System.out.println("Result : " + rnyString.solution("jerry"));
+        System.out.println("Result : " + rnyString.solution("burn"));
+    }
+}
---
+++ src/programmers/coding_basic_training/level0/day18/문자열바꿔서찾기.java
@@ -0,0 +1,40 @@
+package programmers.coding_basic_training.level0.day18;
+
+public class 문자열바꿔서찾기 {
+
+    public int solution(String myString, String pat) {
+
+        // myString 의 "A"를 "B"로,    "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중
+        // pat 이 있으면 1을, 아니면 0을 반환하라
+
+        String before = myString.replace("A", "C");
+        String after = before.replace("B", "A");
+        String answer = after.replace("C", "B");
+
+        if (answer.contains(pat)) {
+
+            return 1;
+
+        } else {
+
+            return 0;
+
+        }
+    }
+
+    public int solution_other(String myString, String pat) {
+
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+
+        return myString.contains(pat) ? 1 : 0;
+
+    }
+
+    public static void main(String[] args) {
+
+        문자열바꿔서찾기 find = new 문자열바꿔서찾기();
+
+        System.out.println("Result : " + find.solution("ABBAA", "AABB"));
+        System.out.println("Result : " + find.solution("ABAB", "ABAB"));
+    }
+}
