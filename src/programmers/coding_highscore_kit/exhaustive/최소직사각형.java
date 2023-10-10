f5f6a815729871e3ab418b656adb199791c9c761
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-11 02:16:20 +0900

    feat : 코딩 기초 트레이닝 19주차

---
+++ src/programmers/coding_highscore_kit/exhaustive/최소직사각형.java
@@ -0,0 +1,27 @@
+package programmers.coding_highscore_kit.exhaustive;
+
+public class 최소직사각형 {
+
+    public int solution(int[][] sizes) {
+
+        // 명합 지갑을 만드는 회사에서 지갑의 크기를 정하려고 한다
+        // 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 반환하라
+
+        // 우선 가장 긴 가로, 세로 길이의 지갑을 만들어보자
+
+        int answer = 0;
+
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        최소직사각형 rectangle = new 최소직사각형();
+
+        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+
+        System.out.println("Result : " + rectangle.solution(sizes));
+    }
+}
