776fcf2b4deb7b81da33325c9e7b9e7e34da72fb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-10-17 23:58:30 +0900

    feat : 코딩 기초 트레이닝 21주차

---
+++ src/programmers/coding_basic_training/level0/day21/정수부분.java
@@ -0,0 +1,17 @@
+package programmers.coding_basic_training.level0.day21;
+
+public class 정수부분 {
+
+    public int solution(double flo) {
+
+        return (int) Math.abs(flo);
+    }
+
+    public static void main(String[] args) {
+
+        정수부분 integer = new 정수부분();
+
+        System.out.println("Result : " + integer.solution(1.42));
+        System.out.println("Result : " + integer.solution(69.32));
+    }
+}
