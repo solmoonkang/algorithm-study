05f433cb67aa88b6377d0c1234fc03c6c15d04a1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-20 18:55:23 +0900

    feat(모의고사): 코딩테스트 고득점 Kit(완전탐색) - 모의고사

--- src/programmers/coding_highscore_kit/exhaustive/모의고사.java
+++ src/programmers/coding_highscore_kit/exhaustive/모의고사.java
@@ -1,40 +1,42 @@
 package programmers.coding_highscore_kit.exhaustive;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 모의고사 {
 
+    /**
+     * 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 한다.
+     * 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍는다.
+     *     1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, ...
+     *     2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, ...
+     *     3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+     * 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 반환하라
+     */
+
     public int[] solution(int[] answers) {
-
-        // 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 한다.
-        // 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍는다.
-            // 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-            // 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-            // 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
-        // 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 반환하라
-
-        int[] answer = {};
-
-        int[] first_retire_answers = {1, 2, 3, 4, 5};
-        int[] second_retire_answers = {2, 1, 2, 3, 2};
-        int[] third_retire_answers = {3, 3, 1, 1, 2};
-
-        int first_count = 0;
-        int second_count = 0;
-        int third_count = 0;
+        // TODO: 완전탐색이 문제가 아니라 전체적으로 코드를 운영하는 능력이 부족하다. 특히 for문을 활용하는 능력이 미숙하여 공부가 필요하다.
+        int[][] patterns = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] correctCount = new int[3];
 
         for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < correctCount.length; j++) {
+                if (answers[i] == patterns[j][i % patterns[j].length]) {
+                    correctCount[j]++;
+                }
+            }
+        }
+        int maxCorrect = Math.max(correctCount[0], Math.max(correctCount[1], correctCount[2]));
 
-            if (answers[i] == first_retire_answers[i]) {
-                first_count++;
-            } else if (answers[i] == second_retire_answers[i]) {
-                second_count++;
-            } else if (answers[i] == third_retire_answers[i]) {
-                third_count++;
+        List<Integer> bestStudents = new ArrayList<>();
+        for (int i = 0; i < correctCount.length; i++) {
+            if (correctCount[i] == maxCorrect) {
+                bestStudents.add(i + 1);
             }
         }
 
-        return answer;
+        return bestStudents.stream().mapToInt(o -> o).toArray();
     }
 
     public static void main(String[] args) {
