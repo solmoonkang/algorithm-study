3e5c981d5559dd3b2ccab71ed53ec0b692a08627
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-28 02:24:57 +0900

    refactor: Revised explanatory comments in classes

--- src/programmers/coding_highscore_kit/exhaustive/최소직사각형.java
+++ src/programmers/coding_highscore_kit/exhaustive/최소직사각형.java
@@ -2,11 +2,11 @@ package programmers.coding_highscore_kit.exhaustive;
 
 public class 최소직사각형 {
 
+    /**
+     * 명합 지갑을 만드는 회사에서 지갑의 크기를 정하려고 한다
+     * 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 반환하라
+     */
     public int solution(int[][] sizes) {
-
-        // 명합 지갑을 만드는 회사에서 지갑의 크기를 정하려고 한다
-        // 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 반환하라
-
         int width = 0;
         int height = 0;
 
