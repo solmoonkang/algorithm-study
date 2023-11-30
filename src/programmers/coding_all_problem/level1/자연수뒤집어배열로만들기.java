a35958f90a66412523c5c367767b1770949bcafa
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-30 21:08:09 +0900

    feat(자연수뒤집어배열로만들기): Implement reverse a natural number into an array in the string in coding test all problems level 1 category

--- src/programmers/coding_all_problem/level1/자연수뒤집어배열로만들기.java
+++ src/programmers/coding_all_problem/level1/자연수뒤집어배열로만들기.java
@@ -2,44 +2,31 @@ package programmers.coding_all_problem.level1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 자연수뒤집어배열로만들기 {
 
     /**
      * 자연수 n 을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요.
-     * 예를 들어, n 이 12345이면, [5, 4, 3, 2, 1] 을 리턴합니다.
      */
 
-    public int[] solution_arrayList(long n) {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        List<Integer> list = new ArrayList<>();
 
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        StringBuilder builder = new StringBuilder(s);
+        builder.reverse();
 
-        // n 이 0 이면, 더 이상의 자릿수가 없다는 의미이다
-        while (n > 0) {
-
-            // 현재 자릿수 추출한다, 예를 들면, 12345 일 경우, 1, 2, 3, 4, 5 의 형식으로 분리한다
-            int currentNum = (int) (n % 10);
-
-            // 현재 자릿수를 arrayList 에 저장한다
-            arrayList.add(currentNum);
-
-            // n 의 일의 자리가 삭제되고, 각 자리의 숫자가 오른쪽에서 왼쪽으로 이동하게 된다
-            n /= 10;    // 즉, n 을 10 으로 나눈 몫으로 업데이트
+        for (int i = 0; i < builder.length(); i++) {
+            char c = builder.charAt(i);
+            int answer = Character.getNumericValue(c);
+            list.add(answer);
         }
-
-        int[] answer = new int[arrayList.size()];
-
-        for (int i = 0; i < arrayList.size(); i++) {
-            answer[i] = arrayList.get(i);
-        }
-
-        return answer;
+        return list.stream().mapToInt(o -> o).toArray();
     }
 
     public static void main(String[] args) {
-
-        자연수뒤집어배열로만들기 reverse = new 자연수뒤집어배열로만들기();
-
-        System.out.println("Answer : " + Arrays.toString(reverse.solution_arrayList(123456789)));
+        자연수뒤집어배열로만들기 reverseNumberIntoArray = new 자연수뒤집어배열로만들기();
+        System.out.println("Result: " + Arrays.toString(reverseNumberIntoArray.solution(12345)));
     }
 }
