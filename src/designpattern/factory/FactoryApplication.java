0c72b65bf7bda83cd60189afb581fd879502ef5b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-02 20:55:30 +0900

    feat(FactoryApplication): Implement FactoryApplication to test CoffeeFactory

---
+++ src/designpattern/factory/FactoryApplication.java
@@ -0,0 +1,12 @@
+package designpattern.factory;
+
+public class FactoryApplication {
+
+    public static void main(String[] args) {
+        Coffee latte = CoffeeFactory.getCoffee("Latte", 4000);
+        Coffee americano = CoffeeFactory.getCoffee("Americano", 3000);
+
+        System.out.println("Factory Latte ::" + latte);
+        System.out.println("Factory Americano ::" + americano);
+    }
+}
