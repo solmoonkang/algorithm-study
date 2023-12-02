1c0209d6e4cd5a2a759cd7daa36e2aae13ae5dd0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-02 20:54:57 +0900

    feat(Latte): Implement Latte as a type of Coffee

---
+++ src/designpattern/factory/Latte.java
@@ -0,0 +1,15 @@
+package designpattern.factory;
+
+public class Latte extends Coffee {
+
+    private int price;
+
+    public Latte(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int getPrice() {
+        return this.price;
+    }
+}
