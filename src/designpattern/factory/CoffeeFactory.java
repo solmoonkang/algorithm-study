c55e993f303f785c4f672879c53c1928d0720388
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-02 20:54:25 +0900

    feat(CoffeeFactory): Implement CoffeeFactory for coffee creation

---
+++ src/designpattern/factory/CoffeeFactory.java
@@ -0,0 +1,12 @@
+package designpattern.factory;
+
+public class CoffeeFactory {
+
+    public static Coffee getCoffee(String type, int price) {
+        if ("Latte".equalsIgnoreCase(type)) return new Latte(price);
+        else if ("Americano".equalsIgnoreCase(type)) return new Americano(price);
+        else {
+            return new DefaultCoffee();
+        }
+    }
+}
