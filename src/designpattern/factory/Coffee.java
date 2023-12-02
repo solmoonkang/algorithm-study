8bb8f8502276c1139a5725cdc417b1b80066dfa6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-02 20:54:04 +0900

    feat(Coffee): Implement abstract Coffee class

---
+++ src/designpattern/factory/Coffee.java
@@ -0,0 +1,11 @@
+package designpattern.factory;
+
+public abstract class Coffee {
+
+    public abstract int getPrice();
+
+    @Override
+    public String toString() {
+        return "Hi this coffee is " + this.getPrice();
+    }
+}
