157fd2f8e148f9c6e14b165f5e6d552f1abe41c7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-03 19:47:34 +0900

    feat(ObserverApplication): Implement ObserverApplication to test Observer pattern

---
+++ src/designpattern/observer/ObserverApplication.java
@@ -0,0 +1,18 @@
+package designpattern.observer;
+
+public class ObserverApplication {
+
+    public static void main(String[] args) {
+        Topic topic = new Topic();
+
+        Observer observerA = new TopicSubscriber("observerA", topic);
+        Observer observerB = new TopicSubscriber("observerB", topic);
+        Observer observerC = new TopicSubscriber("observerC", topic);
+
+        topic.register(observerA);
+        topic.register(observerB);
+        topic.register(observerC);
+
+        topic.postMessage("amumu is op champion!!");
+    }
+}
