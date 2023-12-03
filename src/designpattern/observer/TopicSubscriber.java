71e71c27625c2c5bd729c20760ac65c4f0856353
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-03 19:47:13 +0900

    feat(TopicSubscriber): Implement TopicSubscriber class as Observer in Observer pattern

---
+++ src/designpattern/observer/TopicSubscriber.java
@@ -0,0 +1,19 @@
+package designpattern.observer;
+
+public class TopicSubscriber implements Observer {
+
+    private String name;
+    private Subject topic;
+
+    public TopicSubscriber(String name,
+                           Subject topic) {
+        this.name = name;
+        this.topic = topic;
+    }
+
+    @Override
+    public void update() {
+        String message = (String) topic.getUpdate(this);
+        System.out.println(name + ":: got message >> " + message);
+    }
+}
