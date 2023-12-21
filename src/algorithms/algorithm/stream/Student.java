aada92f22c7a115e41b627ba85095aa70b0389c9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-21 18:13:29 +0900

    feat(Student): Implement isSameIdentity method in Student class

---
+++ src/algorithms/algorithm/stream/Student.java
@@ -0,0 +1,23 @@
+package algorithms.algorithm.stream;
+
+public class Student {
+
+    private final Long identity;
+    private final String name;
+    private final Major major;
+    private final int grade;
+
+    public Student(Long identity,
+                   String name,
+                   Major major,
+                   int grade) {
+        this.identity = identity;
+        this.name = name;
+        this.major = major;
+        this.grade = grade;
+    }
+
+    public boolean isSameIdentity(Long identity) {
+        return this.identity.equals(identity);
+    }
+}
