4e87aebe052987d1575bc546141e4e488551e50d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-02 20:14:28 +0900

    feat: 프로그래머스 레벨 1 모든 문제 문자열 내 마음대로 정렬하기

---
+++ src/programmers/모든문제/level1/문자열내마음대로정렬하기.java
@@ -0,0 +1,41 @@
+package programmers.모든문제.level1;
+
+import java.util.Arrays;
+
+public class 문자열내마음대로정렬하기 {
+
+    /**
+     * [ 문제 설명 ]
+     * 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+     * 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면
+     * 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+     */
+
+    /**
+     * [ 제한 조건 ]
+     * strings는 길이 1 이상, 50이하인 배열입니다.
+     * strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+     * strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+     * 모든 strings의 원소의 길이는 n보다 큽니다.
+     * 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+     */
+    public String[] solution(String[] strings, int n) {
+        // TODO: 람다 표현식을 사용한 정렬 방법에 대해 학습하자.
+        Arrays.sort(strings, (s1, s2) -> {
+            if (s1.charAt(n) != s2.charAt(n)) return Character.compare(s1.charAt(n), s2.charAt(n));
+            else return s1.compareTo(s2);
+        });
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
+        문자열내마음대로정렬하기 customOrder = new 문자열내마음대로정렬하기();
+
+        String[] firstStrings = {"sun", "bed", "car"};
+        System.out.println("customOrder = " + Arrays.toString(customOrder.solution(firstStrings, 1)));
+
+        String[] secondStrings = {"abce", "abcd", "cdx"};
+        System.out.println("customOrder = " + Arrays.toString(customOrder.solution(secondStrings, 2)));
+    }
+}
