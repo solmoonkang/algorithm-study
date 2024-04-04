3edab35ad86cc548d23e57356915328f63c73202
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-04 21:11:41 +0900

    feat: 프로그래머스 레벨 0 PCCE 기출문제 산책

---
+++ src/programmers/기출문제/pcce기출문제/산책.java
@@ -0,0 +1,38 @@
+package programmers.기출문제.pcce기출문제;
+
+import java.util.Arrays;
+
+public class 산책 {
+
+    public int[] solution(String route) {
+        int east = 0;
+        int north = 0;
+        int[] answer = new int [2];
+        for(int i=0; i<route.length(); i++){
+            switch(route.charAt(i)){
+                case 'N':
+                    north++;
+                    break;
+                case 'S':
+                    north--;
+                    break;
+                case 'E':
+                    east++;
+                    break;
+                case 'W':
+                    east--;
+                    break;
+            }
+        }
+        answer[0] = east;
+        answer[1] = north;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        산책 walk = new 산책();
+
+        System.out.println("walk = " + Arrays.toString(walk.solution("NSSNEWWN")));
+        System.out.println("walk = " + Arrays.toString(walk.solution("EESEEWNWSNWWNS")));
+    }
+}
