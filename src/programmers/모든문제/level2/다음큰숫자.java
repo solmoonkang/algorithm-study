751d2a26d1537577dc1e50a63037fd02352a7cfe
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-05 14:17:21 +0900

    feat: 프로그래머스 레벨 2 모든 문제 다음 큰 숫자

--- src/programmers/모든문제/level2/다음큰숫자.java
+++ src/programmers/모든문제/level2/다음큰숫자.java
@@ -64,6 +64,37 @@ public class 다음큰숫자 {
         return n;
     }
 
+
+    /**
+     * [ 문제 설명 ]
+     * 자연수 n이 주어졌을 때, n의 다음 큰 숫자는 다음과 같이 정의 합니다.
+     *
+     * 조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
+     * 조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
+     * 조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
+     * 예를 들어서 78(1001110)의 다음 큰 숫자는 83(1010011)입니다.
+     *
+     * 자연수 n이 매개변수로 주어질 때, n의 다음 큰 숫자를 return 하는 solution 함수를 완성해주세요.
+     */
+
+    /**
+     * [ 제한 사항 ]
+     * n은 1,000,000 이하의 자연수 입니다.
+     */
+    public int solution(int n) {
+        int nowCount = Integer.bitCount(n);
+
+        while (true) {
+            n++;
+
+            int nextCount = Integer.bitCount(n);
+
+            if (nowCount == nextCount) break;
+        }
+
+        return n;
+    }
+
     public static void main(String[] args) {
 
         다음큰숫자 nextBiggerNum = new 다음큰숫자();
@@ -77,5 +108,10 @@ public class 다음큰숫자 {
         System.out.println("===== After The Refactoring =====");
         System.out.println("Result : " + nextBiggerNum.afterRefactoringSolution(78));
         System.out.println("Result : " + nextBiggerNum.afterRefactoringSolution(15));
+
+
+        System.out.println("===== SOLUTION =====");
+        System.out.println("nextBiggerNum = " + nextBiggerNum.solution(78));
+        System.out.println("nextBiggerNum = " + nextBiggerNum.solution(15));
     }
 }
