8aeea7b8d8e4748b02f204734dfd8264a6d316c1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-08-30 18:49:38 +0900

    feat: 코딩테스트 연습 > 연습문제 > 짝수와 홀수

---
+++ src/programmers/모든문제/level1/복습문제/짝수와홀수.java
@@ -0,0 +1,15 @@
+package programmers.모든문제.level1.복습문제;
+
+public class 짝수와홀수 {
+
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        짝수와홀수 problem = new 짝수와홀수();
+
+        System.out.println("problem = " + problem.solution(3));
+        System.out.println("problem = " + problem.solution(4));
+    }
+}
