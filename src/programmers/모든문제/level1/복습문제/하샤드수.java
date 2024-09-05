b97392e64833e3a932514fa7e2f43cfdee0524a8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-09-06 00:50:35 +0900

    feat: 코딩테스트 연습 > 연습문제 > 하샤드 수

---
+++ src/programmers/모든문제/level1/복습문제/하샤드수.java
@@ -0,0 +1,22 @@
+package programmers.모든문제.level1.복습문제;
+
+public class 하샤드수 {
+
+    public boolean solution(int x) {
+        int digits = 0;
+        for (char c : String.valueOf(x).toCharArray()) {
+            digits += Character.getNumericValue(c);
+        }
+
+        return x % digits == 0;
+    }
+
+    public static void main(String[] args) {
+        하샤드수 problem = new 하샤드수();
+
+        System.out.println("problem = " + problem.solution(10));
+        System.out.println("problem = " + problem.solution(12));
+        System.out.println("problem = " + problem.solution(11));
+        System.out.println("problem = " + problem.solution(13));
+    }
+}
