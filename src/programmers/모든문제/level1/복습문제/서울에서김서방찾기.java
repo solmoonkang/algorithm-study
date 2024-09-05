7b9d4c3511469e2a516bc13f1539c038b9d883ae
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-09-06 01:01:55 +0900

    feat: 코딩테스트 연습 > 연습문제 > 서울에서 김서방 찾기

---
+++ src/programmers/모든문제/level1/복습문제/서울에서김서방찾기.java
@@ -0,0 +1,20 @@
+package programmers.모든문제.level1.복습문제;
+
+public class 서울에서김서방찾기 {
+
+    public String solution(String[] seoul) {
+        int location = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) location = i;
+        }
+
+        return "김서방은 " + location + "에 있다";
+    }
+
+    public static void main(String[] args) {
+        서울에서김서방찾기 problem = new 서울에서김서방찾기();
+
+        String[] seoul = {"Jane", "Kim"};
+        System.out.println("problem = " + problem.solution(seoul));
+    }
+}
