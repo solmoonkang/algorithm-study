b48ce8a626199fc79553665d3180037e4def7b79
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-09-12 15:04:52 +0900

    feat: 코딩테스트 연습 > 연습문제 > 문자열 다루기 기본

---
+++ src/programmers/모든문제/level1/복습문제/문자열다루기기본.java
@@ -0,0 +1,19 @@
+package programmers.모든문제.level1.복습문제;
+
+public class 문자열다루기기본 {
+
+    public boolean solution(String s) {
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
+        }
+
+        return s.length() == 4 || s.length() == 6;
+    }
+
+    public static void main(String[] args) {
+        문자열다루기기본 problem = new 문자열다루기기본();
+
+        System.out.println("problem = " + problem.solution("a234"));
+        System.out.println("problem = " + problem.solution("1234"));
+    }
+}
