0be5fdf52a923e6750340cd353b615531327f137
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-09-27 16:23:45 +0900

    feat: 코딩테스트 연습 > 연습문제 > 2016년

---
+++ src/programmers/모든문제/level1/복습문제/년도2016.java
@@ -0,0 +1,25 @@
+package programmers.모든문제.level1.복습문제;
+
+public class 년도2016 {
+
+    public String solution(int a, int b) {
+        int[] daysInMonth = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] weekDays = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+        int totalDays = 0;
+        for (int i = 1; i < a; i++) {
+            totalDays += daysInMonth[i];
+        }
+
+        totalDays += b;
+        int dayOfWeeks = (a + totalDays - 1) % 7;
+
+        return weekDays[dayOfWeeks];
+    }
+
+    public static void main(String[] args) {
+        년도2016 problem = new 년도2016();
+
+        System.out.println("problem = " + problem.solution(5, 24));
+    }
+}
