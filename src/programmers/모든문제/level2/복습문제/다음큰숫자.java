a993e4e7d1214821712534b2a49310c5e03e03d7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-02 16:23:28 +0900

    feat: 코딩테스트 연습 > 연습문제 > 다음 큰 숫자

---
+++ src/programmers/모든문제/level2/복습문제/다음큰숫자.java
@@ -0,0 +1,21 @@
+package programmers.모든문제.level2.복습문제;
+
+public class 다음큰숫자 {
+
+    public int solution(int n) {
+        int targetCount = Integer.bitCount(n);
+        int nextNumber = n + 1;
+
+        while (true) {
+            if (Integer.bitCount(nextNumber) == targetCount) return nextNumber;
+            nextNumber++;
+        }
+    }
+
+    public static void main(String[] args) {
+        다음큰숫자 problem = new 다음큰숫자();
+
+        System.out.println("problem = " + problem.solution(78));
+        System.out.println("problem = " + problem.solution(15));
+    }
+}
