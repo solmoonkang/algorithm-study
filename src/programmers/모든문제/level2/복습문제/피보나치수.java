5a6e8bb38115782dd4ff4582910aa288358f968d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-03 22:36:17 +0900

    feat: 코딩테스트 연습 > 연습문제 > 피보나치 수

---
+++ src/programmers/모든문제/level2/복습문제/피보나치수.java
@@ -0,0 +1,23 @@
+package programmers.모든문제.level2.복습문제;
+
+public class 피보나치수 {
+
+    public int solution(int n) {
+        int[] fibo = new int[n + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+        }
+
+        return fibo[n];
+    }
+
+    public static void main(String[] args) {
+        피보나치수 problem = new 피보나치수();
+
+        System.out.println("problem = " + problem.solution(3));
+        System.out.println("problem = " + problem.solution(5));
+    }
+}
