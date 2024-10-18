5e4251ad92a17bcbf63f3b972644d66d459c49a1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-18 17:05:35 +0900

    test

---
+++ src/programmers/모든문제/level1/복습문제/문자열나누기.java
@@ -0,0 +1,16 @@
+package programmers.모든문제.level1.복습문제;
+
+public class 문자열나누기 {
+
+    public int solution(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        문자열나누기 problem = new 문자열나누기();
+
+        System.out.println("problem = " + problem.solution("banana"));
+        System.out.println("problem = " + problem.solution("abracadabra"));
+        System.out.println("problem = " + problem.solution("aaabbaccccabba"));
+    }
+}
