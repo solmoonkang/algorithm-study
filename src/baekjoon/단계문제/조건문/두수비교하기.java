eafcb466859731f869d9b9f04d3745d105d0a2eb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-27 20:48:09 +0900

    feat: 전체문제 > 조건문 > 두 수 비교하기

---
+++ src/baekjoon/단계문제/조건문/두수비교하기.java
@@ -0,0 +1,16 @@
+package baekjoon.단계문제.조건문;
+
+import java.util.Scanner;
+
+public class 두수비교하기 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (A > B) System.out.println(">");
+        else if (A < B) System.out.println("<");
+        else System.out.println("==");
+    }
+}
