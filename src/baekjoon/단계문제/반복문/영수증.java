9c7278aa3a1fc0836cedc3dd14a97f86b3dea149
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-29 05:29:02 +0900

    feat: 전체문제 > 반복문 > 영수증

---
+++ src/baekjoon/단계문제/반복문/영수증.java
@@ -0,0 +1,23 @@
+package baekjoon.단계문제.반복문;
+
+import java.util.Scanner;
+
+public class 영수증 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int N = scanner.nextInt();
+
+        int total = 0;
+        for (int i = 0; i < N; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            total += a * b;
+        }
+
+        if (X == total) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
