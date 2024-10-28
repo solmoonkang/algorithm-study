d11d53cb2d2a2b09196409761564ce703e9bfa5f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-29 05:19:03 +0900

    feat: 전체문제 > 반복문 > 구구단

--- src/baekjoon/단계문제/반복문/구구단.java
+++ src/baekjoon/단계문제/반복문/구구단.java
@@ -1,15 +1,15 @@
 package baekjoon.단계문제.반복문;
 
+import java.util.Scanner;
+
 public class 구구단 {
 
-    // N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
-
     public static void main(String[] args) {
-        int N = 2;
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
 
         for (int i = 1; i <= 9; i++) {
             System.out.println(N + " * " + i + " = " + N * i);
         }
-        System.out.println();
     }
 }
