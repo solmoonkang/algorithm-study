0d5ccfc6363eb3751b9e25c6330494848c856bfb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-28 23:17:26 +0900

    feat: 전체문제 > 반복문 > 합

---
+++ src/baekjoon/단계문제/반복문/합.java
@@ -0,0 +1,18 @@
+package baekjoon.단계문제.반복문;
+
+import java.util.Scanner;
+
+public class 합 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            answer += i;
+        }
+
+        System.out.println(answer);
+    }
+}
