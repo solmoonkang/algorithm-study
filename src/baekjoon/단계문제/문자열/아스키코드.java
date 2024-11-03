b6b06f948946525036c15d403f8600736643b5ce
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-11-03 14:49:41 +0900

    feat: 전체문제 > 문자열 > 아스키코드

---
+++ src/baekjoon/단계문제/문자열/아스키코드.java
@@ -0,0 +1,22 @@
+package baekjoon.단계문제.문자열;
+
+import java.io.*;
+
+public class 아스키코드 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String I = bufferedReader.readLine();
+
+        for (char ch : I.toCharArray()) {
+            int ascii = (int) ch;
+            bufferedWriter.write(String.valueOf(ascii));
+        }
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
