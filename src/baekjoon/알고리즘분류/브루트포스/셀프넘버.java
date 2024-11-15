6eca36088771e0a30f4109cdb347658d83bd8e59
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-11-15 13:52:03 +0900

    feat: 전체문제 > 알고리즘 분류 > 정렬 > 셀프 넘버

---
+++ src/baekjoon/알고리즘분류/브루트포스/셀프넘버.java
@@ -0,0 +1,36 @@
+package baekjoon.알고리즘분류.브루트포스;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class 셀프넘버 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<Integer> generateNumbers = new HashSet<>();
+
+        for (int i = 1; i <= 10000; i++) {
+            generateNumbers.add(d(i));
+        }
+
+        for (int i = 1; i <= 10000; i++) {
+            if (!generateNumbers.contains(i)) bufferedWriter.write(i + "\n");
+        }
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+
+    public static int d(int N) {
+        int sum = N;
+
+        while (N > 0) {
+            sum += N % 10;
+            N /= 10;
+        }
+
+        return sum;
+    }
+}
