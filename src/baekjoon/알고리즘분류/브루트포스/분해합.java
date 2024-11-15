41baa791c4fff2522d3895be9ac92855689497f5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-11-15 14:44:03 +0900

    feat: 전체문제 > 알고리즘 분류 > 정렬 > 분해합

---
+++ src/baekjoon/알고리즘분류/브루트포스/분해합.java
@@ -0,0 +1,37 @@
+package baekjoon.알고리즘분류.브루트포스;
+
+import java.io.*;
+
+public class 분해합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        int decompositionSum = 0;
+        for (int M = 1; M < N; M++) {
+            if (M + getDecompositionSum(M) == N) {
+                decompositionSum = M;
+                break;
+            }
+        }
+
+        bufferedWriter.write(String.valueOf(decompositionSum));
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+
+    public static int getDecompositionSum(int number) {
+        int sumOfDigits = 0;
+
+        while (number > 0) {
+            sumOfDigits += number % 10;
+            number /= 10;
+        }
+
+        return sumOfDigits;
+    }
+}
