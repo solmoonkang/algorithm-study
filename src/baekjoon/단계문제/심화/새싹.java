9f60236351d6aa89a3a1fcf28a44e3bc2da98c74
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-11-17 20:13:08 +0900

    feat: 전체문제 > 단계문제 > 심화 > 새싹

---
+++ src/baekjoon/단계문제/심화/새싹.java
@@ -0,0 +1,19 @@
+package baekjoon.단계문제.심화;
+
+import java.io.*;
+
+public class 새싹 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bufferedWriter.write("         ,r'\"7\n");
+        bufferedWriter.write("r`-_   ,'  ,/\n");
+        bufferedWriter.write(" \\. \". L_r'\n");
+        bufferedWriter.write("   `~\\/\n");
+        bufferedWriter.write("      |\n");
+        bufferedWriter.write("      |");
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
