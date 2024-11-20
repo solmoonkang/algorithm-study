fd36a91e0b8ff19a7417793c3007e5db9cf8d5ca
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-11-20 13:11:40 +0900

    feat: 전체문제 > 알고리즘 분류 > 자료구조 > AC

---
+++ src/baekjoon/알고리즘분류/자료구조/AC.java
@@ -0,0 +1,67 @@
+package baekjoon.알고리즘분류.자료구조;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class AC {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String commandSequence = bufferedReader.readLine();
+            int N = Integer.parseInt(bufferedReader.readLine());
+
+            String[] numberStrings = bufferedReader.readLine().replaceAll("[\\[\\] ]", "").split(",");
+
+            Deque<Integer> numbersDeque = new ArrayDeque<>();
+            for (String number : numberStrings) {
+                if (!number.isEmpty()) numbersDeque.offerLast(Integer.parseInt(number));
+            }
+
+            String result = executeCommandsOnDeque(commandSequence, numbersDeque);
+            bufferedWriter.write(result + "\n");
+        }
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+
+    public static String executeCommandsOnDeque(String commandSequence, Deque<Integer> numberDeque) {
+        boolean isReversed = false;
+
+        for (char command : commandSequence.toCharArray()) {
+            if (command == 'R') {
+                isReversed = !isReversed;
+            } else if (command == 'D') {
+                if (numberDeque.isEmpty()) return "error";
+
+                if (isReversed) numberDeque.pollLast();
+                else numberDeque.pollFirst();
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        if (isReversed) {
+            while (!numberDeque.isEmpty()) {
+                stringBuilder.append(numberDeque.pollLast());
+                if (!numberDeque.isEmpty()) stringBuilder.append(",");
+            }
+        } else {
+            while (!numberDeque.isEmpty()) {
+                stringBuilder.append(numberDeque.pollFirst());
+                if (!numberDeque.isEmpty()) stringBuilder.append(",");
+            }
+        }
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
