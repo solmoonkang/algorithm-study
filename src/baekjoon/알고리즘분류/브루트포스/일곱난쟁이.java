968c9d6e59cc67b538511a8019a09678677318cb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-11-21 16:31:00 +0900

    feat: 전체문제 > 알고리즘분류 > 브루트포스 > 일곱 난쟁이

---
+++ src/baekjoon/알고리즘분류/브루트포스/일곱난쟁이.java
@@ -0,0 +1,40 @@
+package baekjoon.알고리즘분류.브루트포스;
+
+import java.io.*;
+
+public class 일곱난쟁이 {
+
+    private static final int N = 9;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] dwarfHeights = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            dwarfHeights[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int heightSum = 0;
+
+                for (int k = 0; k < N; k++) {
+                    if (k != i && k != j) heightSum += dwarfHeights[k];
+                }
+
+                if (heightSum == 100) {
+                    for (int k = 0; k < N; k++) {
+                        if (k != i && k != j) bufferedWriter.write(dwarfHeights[k] + "\n");
+                    }
+
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    bufferedReader.close();
+                    return;
+                }
+            }
+        }
+    }
+}
