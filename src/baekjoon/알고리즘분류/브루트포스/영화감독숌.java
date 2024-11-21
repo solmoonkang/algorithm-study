9b061c12465d63c160eab93cb5babc2675f21b3e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-11-21 14:49:44 +0900

    feat: 전체문제 > 알고리즘분류 > 브루트포스 > 영화감독 숌

---
+++ src/baekjoon/알고리즘분류/브루트포스/영화감독숌.java
@@ -0,0 +1,32 @@
+package baekjoon.알고리즘분류.브루트포스;
+
+import java.io.*;
+
+public class 영화감독숌 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        int movieTitle = 0;
+        int number = 666;
+
+        while (true) {
+            if (String.valueOf(number).contains("666")) movieTitle++;
+
+            if (movieTitle == N) {
+                bufferedWriter.write(String.valueOf(number));
+                break;
+            }
+
+            number++;
+        }
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
