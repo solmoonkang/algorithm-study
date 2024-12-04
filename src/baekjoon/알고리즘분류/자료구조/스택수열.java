d835e7d5b7b18e9e7843f506d7747b9633630c75
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-12-04 15:26:57 +0900

    feat: 전체문제 > 알고리즘분류 > 자료구조 > 스택 수열

---
+++ src/baekjoon/알고리즘분류/자료구조/스택수열.java
@@ -0,0 +1,46 @@
+package baekjoon.알고리즘분류.자료구조;
+
+import java.io.*;
+import java.util.*;
+
+public class 스택수열 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        Deque<Integer> deque = new LinkedList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        List<Integer> numberList = new ArrayList<>();
+
+        int currentNumber = 1;
+
+        for (int i = 0; i < N; i++) {
+            numberList.add(Integer.parseInt(bufferedReader.readLine()));
+        }
+
+        for (int number : numberList) {
+            while (currentNumber <= number) {
+                deque.offerLast(currentNumber++);
+                stringBuilder.append("+\n");
+            }
+
+            if (deque.isEmpty() || !Objects.equals(deque.peekLast(), number)) {
+                bufferedWriter.write("NO\n");
+                bufferedWriter.flush();
+                return;
+            }
+
+            deque.pollLast();
+            stringBuilder.append("-\n");
+        }
+
+        bufferedWriter.write(stringBuilder.toString());
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
