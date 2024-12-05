39235d07c00cf22a135e912208514976d718bd49
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-12-05 18:08:01 +0900

    feat: 전체문제 > 알고리즘고득점KIT > 다이나믹프로그래밍 > N으로 표현

--- src/programmers/알고리즘고득점KIT/Dynamic/N으로표현.java
+++ src/programmers/알고리즘고득점KIT/Dynamic/N으로표현.java
@@ -1,29 +1,39 @@
 package programmers.알고리즘고득점KIT.Dynamic;
 
+import java.util.Arrays;
+
 public class N으로표현 {
 
-    /**
-     * [ 문제 설명 ]
-     * 아래와 같이 5와 사칙연산만으로 12를 표현할 수 있습니다.
-     * 12 = 5 + 5 + (5 / 5) + (5 / 5)
-     * 12 = 55 / 5 + 5 / 5
-     * 12 = (55 + 5) / 5
-     * 5를 사용한 횟수는 각각 6,5,4 입니다. 그리고 이중 가장 작은 경우는 4입니다.
-     * 이처럼 숫자 N과 number가 주어질 때, N과 사칙연산만 사용해서 표현 할 수 있는 방법 중 N 사용횟수의 최솟값을 반환하라
-     */
+    private static final int MAX_NUMBER = 32000;
 
-    /**
-     * [ 문제 조건 ]
-     * N은 1 이상 9 이하입니다.
-     * number는 1 이상 32,000 이하입니다.
-     * 수식에는 괄호와 사칙연산만 가능하며 나누기 연산에서 나머지는 무시합니다.
-     * 최솟값이 8보다 크면 -1을 return 합니다.
-     */
-
+    // TODO: 문제가 정상적으로 동작하지 않는다. 점화식 부분을 다시 한 번 구현해보자.
     public int solution(int N, int number) {
-        int count = 0;
+        int[] dynamicArray = new int[MAX_NUMBER + 1];
 
-        return count;
+        Arrays.fill(dynamicArray, 9);
+        dynamicArray[0] = 0;
+        dynamicArray[N] = 1;
+
+        for (int k = 1; k <= 8; k++) {
+            int currentN = 0;
+            for (int j = 0; j < k; j++) {
+                currentN = currentN * 10 + N;
+            }
+
+            if (currentN <= MAX_NUMBER) dynamicArray[currentN] = Math.min(dynamicArray[currentN], k);
+        }
+
+        for (int i = 1; i <= MAX_NUMBER; i++) {
+            for (int j = 1; j < i; j++) {
+                dynamicArray[i] = Math.min(dynamicArray[i], dynamicArray[j] + dynamicArray[i - j]);
+
+                if (i % j == 0) dynamicArray[i] = Math.min(dynamicArray[i], dynamicArray[j] + dynamicArray[i / j]);
+                if (i - j >= 0) dynamicArray[i] = Math.min(dynamicArray[i], dynamicArray[j] + dynamicArray[i - j]);
+                if (j > 0 && i % j == 0) dynamicArray[i] = Math.min(dynamicArray[i], dynamicArray[j] + dynamicArray[i / j]);
+            }
+        }
+
+        return (dynamicArray[number] > 8) ? -1 : dynamicArray[number];
     }
 
     public static void main(String[] args) {
