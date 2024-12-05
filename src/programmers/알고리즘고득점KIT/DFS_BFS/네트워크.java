e63d7bce543de7644797fc49706c4e325027c3dd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-12-05 18:52:44 +0900

    feat: 전체문제 > 알고리즘고득점KIT > DFS & BFS > 네트워크

--- src/programmers/알고리즘고득점KIT/DFS_BFS/네트워크.java
+++ src/programmers/알고리즘고득점KIT/DFS_BFS/네트워크.java
@@ -2,25 +2,38 @@ package programmers.알고리즘고득점KIT.DFS_BFS;
 
 public class 네트워크 {
 
+    private boolean[] visited;
+
     public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
 
-        // 컴퓨터 A와 컴퓨터 B가 직접적으로 연결되어있고, 컴퓨터 B와 컴퓨터 C가 직접적으로 연결되어 있을 때,
+        int count = 0;
 
-        // 컴퓨터 A와 컴퓨터 C도 간접적으로 연결되어 정보를 교환할 수 있다 -> 따라서 컴퓨터 A, B, C는 모두 같은 네트워크 상에 있다고 할 수 있다
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                DFS(i, n, computers);
+                count++;
+            }
+        }
 
-        // 컴퓨터의 개수 N개, 연결에 대한 정보가 담긴 2차원 배열 Computers 가 매개변수로 주어질 때, 네트워크의 개수를 반환하라
+        return count;
+    }
 
-        int answer = 0;
+    private void DFS(int x, int n, int[][] computers) {
+        visited[x] = true;
 
-        return answer;
+        for (int y = 0; y < n; y++) {
+            if (computers[x][y] == 1 && !visited[y]) DFS(y, n, computers);
+        }
     }
 
     public static void main(String[] args) {
-
         네트워크 network = new 네트워크();
 
-        int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int[][] computers1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        System.out.println("network = " + network.solution(3, computers1));
 
-        System.out.println("Result : " + network.solution(3, computers));
+        int[][] computers2 = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+        System.out.println("network = " + network.solution(3, computers2));
     }
 }
