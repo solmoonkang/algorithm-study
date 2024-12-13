6fa13f100e298ce91c6a2faff27a01e42708ed69
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-12-13 22:54:58 +0900

    feat: 전체문제 > 알고리즘분류 > 문자열 > A+B 6

---
+++ src/baekjoon/알고리즘분류/문자열/A더하기B_6.java
@@ -0,0 +1,26 @@
+package baekjoon.알고리즘분류.문자열;
+
+import java.io.*;
+
+public class A더하기B_6 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            String[] numbers = bufferedReader.readLine().split(",");
+            int aPlusB = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+            stringBuilder.append(aPlusB).append("\n");
+        }
+
+        bufferedWriter.write(stringBuilder.toString());
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
