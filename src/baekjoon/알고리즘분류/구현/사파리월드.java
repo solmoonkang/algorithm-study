ccddc63c6cd7cd5e2c27fce9ccb9ac29e339c911
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-12-15 19:38:43 +0900

    feat: 전체문제 > 알고리즘분류 > 구현 > 사파리 월드

---
+++ src/baekjoon/알고리즘분류/구현/사파리월드.java
@@ -0,0 +1,23 @@
+package baekjoon.알고리즘분류.구현;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 사파리월드 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        long N = Long.parseLong(stringTokenizer.nextToken());
+        long M = Long.parseLong(stringTokenizer.nextToken());
+
+        long differenceInPopularity = Math.abs(N - M);
+        bufferedWriter.write(String.valueOf(differenceInPopularity));
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
