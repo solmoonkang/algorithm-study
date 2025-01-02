a74993cadc10ccfe7570bcf6a0aa85e4ad3fb3b9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-02 12:55:50 +0900

    feat: 전체문제 > 알고리즘분류 > 해시를사용한집합과맵 > 복습문제 > 인사성 밝은 곰곰이

---
+++ src/baekjoon/알고리즘분류/해시를사용한집합과맵/복습문제/인사성밝은곰곰이.java
@@ -0,0 +1,36 @@
+package baekjoon.알고리즘분류.해시를사용한집합과맵.복습문제;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 인사성밝은곰곰이 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        Map<String, Boolean> openChatEntranceMap = new HashMap<>();
+
+        int gomGomEmoticonUsage = 0;
+        for (int i = 0; i < N; i++) {
+            String nickname = bufferedReader.readLine();
+
+            if (nickname.equals("ENTER")) {
+                openChatEntranceMap.clear();
+            } else {
+                if (!openChatEntranceMap.containsKey(nickname)) {
+                    gomGomEmoticonUsage++;
+                    openChatEntranceMap.put(nickname, true);
+                }
+            }
+        }
+
+        bufferedWriter.write(String.valueOf(gomGomEmoticonUsage));
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
