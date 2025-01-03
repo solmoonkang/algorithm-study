43fb06ba10e4a6310323bdd0b5c88d1e5ef6f9f7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-03 18:55:34 +0900

    feat: 전체문제 > 알고리즘고득점KIT > 해시 > 폰켓몬

---
+++ src/programmers/알고리즘고득점KIT/해시/폰켓몬.java
@@ -0,0 +1,29 @@
+package programmers.알고리즘고득점KIT.해시;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 폰켓몬 {
+
+    public int solution(int[] nums) {
+        Set<Integer> pokémonSet = new HashSet<>();
+        for (int number : nums) pokémonSet.add(number);
+
+        return Math.min(pokémonSet.size(), nums.length / 2);
+    }
+
+    public static void main(String[] args) {
+        폰켓몬 problem = new 폰켓몬();
+
+        int[] nums1 = {3, 1, 2, 3};
+        System.out.println("problem = " + problem.solution(nums1));
+
+
+        int[] nums2 = {3, 3, 3, 2, 2, 4};
+        System.out.println("problem = " + problem.solution(nums2));
+
+
+        int[] nums3 = {3, 3, 3, 2, 2, 2};
+        System.out.println("problem = " + problem.solution(nums3));
+    }
+}
