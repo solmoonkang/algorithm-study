849e1814edbadb8c6444d2415ee05b7c37f83005
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-11 20:32:20 +0900

    feat: 전체문제 > 알고리즘분류 > 구현 > 방 번호

---
+++ src/baekjoon/알고리즘분류/구현/방번호.java
@@ -0,0 +1,29 @@
+package baekjoon.알고리즘분류.구현;
+
+import java.io.*;
+
+public class 방번호 {
+
+    private static final int NUMBER_RANGE = 10;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String N = bufferedReader.readLine();
+
+        int[] numbers = new int[NUMBER_RANGE];
+
+        for (char digit : N.toCharArray()) numbers[digit - '0']++;
+        int sixNineCount = numbers[6] + numbers[9];
+        int setsNeeded = (sixNineCount + 1) / 2;
+
+        for (int i = 0; i < NUMBER_RANGE; i++) {
+            if (i != 6 && i != 9) setsNeeded = Math.max(setsNeeded, numbers[i]);
+        }
+        bufferedWriter.write(String.valueOf(setsNeeded));
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
