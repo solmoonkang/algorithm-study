5b32ebe89cf728ccd0742d075cfa28ed4c7a432f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-02-23 17:11:35 +0900

    feat: 정렬 없이 한 번의 순회로 최솟값, 최댓값 갱신

---
+++ algorithm-programmers/src/모든문제/level2/재복습문제/최댓값과최솟값.java
@@ -0,0 +1,40 @@
+package 모든문제.level2.재복습문제;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 최댓값과최솟값 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String numbers = bufferedReader.readLine();
+		bufferedReader.close();
+
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		String minAndMaxNumber = getMinAndMaxNumbers(numbers);
+		bufferedWriter.write(minAndMaxNumber);
+		bufferedWriter.flush();
+		bufferedWriter.close();
+	}
+
+	private static String getMinAndMaxNumbers(String numbers) {
+		String[] splitNumbers = numbers.split(" ");
+
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+
+		for (String numStr : splitNumbers) {
+			int num = Integer.parseInt(numStr);
+
+			if (num < min)
+				min = num;
+			if (num > max)
+				max = num;
+		}
+
+		return min + " " + max;
+	}
+}
